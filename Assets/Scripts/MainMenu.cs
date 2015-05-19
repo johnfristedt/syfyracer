f046de3fb3953ded4325c911b07dcbda8474a2dd
johnfristedt <john@kodalagom.se>
2015-05-19 16:17:18 +0200

    End of day

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour {
+
+    public Button LevelSelectButton;
+
+	// Use this for initialization
+	void Start () 
+    {
+        LevelSelectButton.onClick.AddListener(() => LevelSelect());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void LevelSelect()
+    {
+        Application.LoadLevel(1);
+    }
+}
